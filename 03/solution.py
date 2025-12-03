683d095c15a4844a1e2abe5557b16bdf5ab0980c
Alex <lxnn.git@gmail.com>
2025-12-03 10:11:21 +0000

    Add day 03

---
+++ 03/solution.py
@@ -0,0 +1,25 @@
+import sys
+
+with open(sys.argv[1]) as file:
+    banks = file.read().splitlines()
+
+def max_joltage_p1(bank):
+    first_digit = max(bank[:-1])
+    index = bank.index(first_digit)
+    second_digit = max(bank[index+1:])
+    return int(first_digit + second_digit)
+
+p1 = sum(max_joltage_p1(bank) for bank in banks)
+print(p1)
+
+def max_joltage_p2(bank, n=12):
+    assert len(bank) >= n
+    if n == 0:
+        return 0
+    first_digit = max(bank[:len(bank)-(n-1)])
+    remaining = bank[bank.index(first_digit)+1:]
+    return 10**(n-1) * int(first_digit) + max_joltage_p2(remaining, n=n-1)
+
+p2 = sum(max_joltage_p2(bank) for bank in banks)
+print(p2)
+

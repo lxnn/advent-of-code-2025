c2e6d9b14cae19299519e8097292f03acfa5fea9
Alex <lxnn.git@gmail.com>
2025-12-06 09:33:20 +0000

    Add day 6

---
+++ 06/solution.py
@@ -0,0 +1,32 @@
+import sys, operator, functools, itertools
+
+with open(sys.argv[1]) as file:
+    lines = file.read().splitlines()
+
+OPS = {'+': operator.add, '*': operator.mul}
+
+results_p1 = [
+    functools.reduce(OPS[opname], map(int, numbers))
+    for *numbers, opname in zip(*(line.split() for line in lines))
+]
+
+p1 = sum(results_p1)
+print(p1)
+
+problems = [
+    (
+        OPS[first[-1]],
+        [int(first[:-1]), *map(int, rest)]
+    )
+    for is_separator, (first, *rest) in itertools.groupby(
+        (''.join(column) for column in zip(*lines)),
+        lambda column: column.strip() == '',
+    )
+    if not is_separator
+]
+results_p2 = [
+    functools.reduce(op, numbers)
+    for op, numbers in problems
+]
+p2 = sum(results_p2)
+print(p2)
